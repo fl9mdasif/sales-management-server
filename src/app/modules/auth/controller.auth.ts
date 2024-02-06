c5949aa1bd6cbaa2c0320ece95264863ade5f955
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2024-02-06 21:26:10 +0600

    res error

--- src/app/modules/auth/controller.auth.ts
+++ src/app/modules/auth/controller.auth.ts
@@ -2,18 +2,21 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import { response } from '../../utils/sendResponse';
 import { authServices } from './service.auth';
-import config from '../../config';
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await authServices.loginUser(req.body);
 
   const { data, accessToken, refreshToken } = result;
 
-  // console.log(accessToken, refreshToken);
-  res.cookie('refreshToken', refreshToken, {
-    secure: config.NODE_ENV === 'production',
-    httpOnly: true,
-  });
+  console.log('res', result);
+  // // console.log(accessToken, refreshToken);
+  // res.cookie('refreshToken', refreshToken, {
+  //   secure: config.NODE_ENV === 'production',
+  //   httpOnly: true,
+  // });
+  // Set accessToken and refreshToken in localStorage
+  localStorage.setItem('accessToken', accessToken);
+  localStorage.setItem('refreshToken', refreshToken);
 
   response.createSendResponse(res, {
     statusCode: httpStatus.OK,
@@ -44,17 +47,20 @@ const changePassword = catchAsync(async (req, res) => {
 
 // refresh
 const refreshToken = catchAsync(async (req, res) => {
-  const { refreshToken } = req.cookies;
+  // Retrieve refreshToken
+  // const { refreshToken } = req.cookies;
+  const refreshToken = localStorage.getItem('refreshToken') as string | null;
+  if (refreshToken) {
+    // console.log('r', refreshToken);
+    const result = await authServices.refreshToken(refreshToken);
 
-  // console.log('r', refreshToken);
-  const result = await authServices.refreshToken(refreshToken);
-
-  response.createSendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Refresh token is retrieved successfully!',
-    data: result,
-  });
+    response.createSendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Refresh token is retrieved successfully!',
+      data: result,
+    });
+  }
 });
 
 export const authControllers = {
